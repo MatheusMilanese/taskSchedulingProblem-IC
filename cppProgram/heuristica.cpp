3aa0407a0d16560eb1226bff3a7b84cf1d05637b
Matheus Milanese <matheus.milanese@gmail.com>
2023-05-03 18:31:15 -0300

    update: algoritmo de timing adaptado

--- cppProgram/heuristica.cpp
+++ cppProgram/heuristica.cpp
@@ -6,21 +6,58 @@ using namespace std;
              F U N Ç Õ E S     I N T E R N A S
   #######################################################*/
 
-
 vector<vector<int>> GerarTempoDeConclusao(vector<vector<int>> machineJobTime, vector<int> deliveryDates, vector<int> indexOrder){
-    vector<vector<int>> tempoDeConclusao(machineJobTime.size(), vector<int>(machineJobTime[1].size()));
-
+    int qtdJobs = machineJobTime.size();
+    int qtdMaquinas = machineJobTime[1].size();
+    vector<vector<int>> tempoDeConclusao(qtdJobs, vector<int>(qtdMaquinas));
 
     tempoDeConclusao[1][1] = machineJobTime[indexOrder[1]][1];
-    for(int i = 1; i < machineJobTime[1].size(); i++){
+    for(int i = 1; i < qtdMaquinas; i++){
         tempoDeConclusao[1][i] = tempoDeConclusao[1][i-1] + machineJobTime[indexOrder[1]][i];
     }
-    for(int i = 2; i < machineJobTime.size(); i++){
+    for(int i = 2; i < qtdJobs; i++){
         tempoDeConclusao[i][1] = tempoDeConclusao[i-1][1] + machineJobTime[indexOrder[i]][1];
-        for(int j = 2; j < machineJobTime[1].size(); j++){
+        for(int j = 2; j < qtdMaquinas; j++){
             tempoDeConclusao[i][j] = max(tempoDeConclusao[i-1][j], tempoDeConclusao[i][j-1]) + machineJobTime[indexOrder[i]][j];
         }
     }
+
+    if(tempoDeConclusao[qtdJobs-1][qtdMaquinas-1] < deliveryDates[indexOrder[qtdJobs-1]]){
+        tempoDeConclusao[qtdJobs-1][qtdMaquinas-1] = deliveryDates[indexOrder[qtdJobs-1]];
+    }
+
+    for(int i = qtdJobs-2; i > 0; i--){
+        // verifica se a tarefa i está adiantada
+        if(tempoDeConclusao[i][qtdMaquinas-1] < deliveryDates[indexOrder[i]]){ 
+            // verifica se a tarefa i+1 é processada depois da data de entrega da tarefa i
+            if(tempoDeConclusao[i+1][qtdMaquinas-1]-machineJobTime[i+1][qtdMaquinas-1] >= deliveryDates[indexOrder[i]]){ 
+                tempoDeConclusao[i][qtdMaquinas-1] = deliveryDates[indexOrder[i]];
+            }
+            else{
+                int k = i;
+                int qtdTarefasAdiantadas = 1;
+                int qtdTarefasJuntas = 1;
+                cout << k << " " << tempoDeConclusao[k+1][qtdMaquinas-1] << " " << machineJobTime[indexOrder[k+1]][qtdMaquinas-1] << " " << tempoDeConclusao[k][qtdMaquinas-1] << endl;
+                while(k < qtdJobs-1 && tempoDeConclusao[k+1][qtdMaquinas-1]-machineJobTime[indexOrder[k+1]][qtdMaquinas-1] == tempoDeConclusao[k][qtdMaquinas-1]){
+                    cout << k << " ";
+                    if(tempoDeConclusao[k+1][qtdMaquinas-1] < deliveryDates[indexOrder[k+1]]){
+                        qtdTarefasAdiantadas++;
+                    }
+                    k++;
+                    qtdTarefasJuntas++;
+                }
+                cout << endl;
+                if(qtdTarefasAdiantadas > qtdTarefasJuntas/2){
+                    for(int j = i; j <= k; j++){
+                        tempoDeConclusao[j][qtdMaquinas-1]++;
+                    }
+                    i++;
+                }
+            }
+        }
+        
+    }
+    
     cout << "######## tempo de conclusao #########\n";
     for(int i = 1; i < tempoDeConclusao.size(); i++)
         for(int j = 1; j < tempoDeConclusao[1].size(); j++)
